bb99658e4c9546e0b3f8a96496144658229e51c7
Krishna Myneni <krishna.myneni@ccreweb.org>
2020-02-06 17:25:03 -0600

    Updated some opcodes for consistency with kForth 2.1.x.
    	modified:   src/fbc.h

--- src/fbc.h
+++ src/fbc.h
@@ -2,7 +2,7 @@
 //
 //  Forth Byte Codes (FBC) for kForth
 //
-//  Copyright (c) 1996--2018 Krishna Myneni,
+//  Copyright (c) 1996--2020 Krishna Myneni,
 //    <krishna.myneni@ccreweb.org>
 //
 //  This software is provided under the terms of the GNU
@@ -184,7 +184,7 @@
 #define OP_TWOMINUS             169
 #define OP_CFETCH               170
 #define OP_CSTORE               171
-#define OP_WFETCH               172
+#define OP_SWFETCH              172
 #define OP_WSTORE               173
 #define OP_DFFETCH              174
 #define OP_DFSTORE              175
@@ -313,14 +313,6 @@
 #define OP_NUMBERQUERY          298
 #define OP_SLITERAL             299
 
-#define OP_DIV_FLOORED          300
-#define OP_MOD_FLOORED          301
-#define OP_SLASHMOD_FLOORED     302
-#define OP_STARSLASH_FLOORED    303
-#define OP_STARSLASHMOD_FLOORED 304
-#define OP_MSLASH_FLOORED       305
-#define OP_MSTARSLASH_FLOORED   306
-
 #define OP_SPACE                310
 #define OP_BLANK                311
 #define OP_SLASHSTRING          312
@@ -361,6 +353,9 @@
 #define OP_BRACKET_ELSE         353
 #define OP_BRACKET_THEN         354
 
+#define OP_CATCH                356
+#define OP_THROW                357
+
 #define OP_PRECISION            360
 #define OP_SET_PRECISION        361
 #define OP_REPRESENT            362
@@ -383,5 +378,23 @@
 #define OP_ZMUL                 378
 #define OP_ZDIV                 379
 #define OP_ZABS                 380
+
+#define OP_BOOL_NOT             400
+#define OP_BOOL_AND             401
+#define OP_BOOL_OR              402
+#define OP_BOOL_XOR             403
+#define OP_BOOLEAN_QUERY        404
+#define OP_UWFETCH              405
+#define OP_ULFETCH              406
+#define OP_SLFETCH              407
+#define OP_LSTORE               408
+
+#define OP_DIV_FLOORED          410
+#define OP_MOD_FLOORED          411
+#define OP_SLASHMOD_FLOORED     412
+#define OP_STARSLASH_FLOORED    413
+#define OP_STARSLASHMOD_FLOORED 414
+#define OP_MSLASH_FLOORED       415
+#define OP_MSTARSLASH_FLOORED   416
 
 #endif
