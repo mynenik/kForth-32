3c56f49d1df80564bcfe643e62fdb63268c08911
Krishna Myneni <krishna.myneni@ccreweb.org>
2021-05-28 06:58:39 -0500

    Renamed branch 1.6 to 1.x; Updated ver. to 1.7.0
     On branch 1.x
    
     Changes to be committed:
    	modified:   src/Makefile
    	modified:   src/kforth.cpp

--- src/kforth.cpp
+++ src/kforth.cpp
@@ -79,7 +79,7 @@ int main(int argc, char *argv[])
 
     if (argc < 2) {
 	cout << "kForth-32 v " << version << "\t (Build: " << build << ")" << endl;
-	cout << "Copyright (c) 1998--2020 Krishna Myneni" << endl;
+	cout << "Copyright (c) 1998--2021 Krishna Myneni" << endl;
         cout << "Contributions by: dpw gd mu bk abs tn cmb bg dnw" << endl;
 	cout << "Provided under the GNU Affero General Public License, v3.0 or later" 
 	  << endl << endl;
